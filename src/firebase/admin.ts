8ceeb33f49633d1854c8292af6586c69a39066a2
Bivek Gharti <gcbibek3353@gmail.com>
2025-04-06 09:26:12 +0530

    little change to format env variable

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -8,7 +8,7 @@ const init_firebase_admin = () => {
         credential: cert({
             projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
             clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+            privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         })
     });
  
